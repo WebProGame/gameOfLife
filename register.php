41af0a8a631ac8faed46520fb9268d098ee9f07e
Raymond Hernandez <67213974+rayherna18@users.noreply.github.com>
2023-04-21 16:20:03 -0400

    Add files via upload

--- register.php
+++ register.php
@@ -5,46 +5,57 @@
   $score = 0;
   $time = 0;
   $missing = array();
-
+    // The code within only activates when the form button is POSTED.
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate input data
+
+    // Checks if name is empty after removing white space from username input
     if (empty(trim($_POST["name"]))) {
+    // If it is then a message is added to missing array
       $missing[] = "Enter a username!";
     } else {
+    // Otherwise if the username is present, it is added from POST array to $username variable
       $username = trim($_POST["name"]);
     }
 
+    // Checks if password is empty after removing white space from password input
     if (empty(trim($_POST["pass"]))) {
+    // If it is then a message is added to missing array
       $missing[] = "Enter a password!";
     } else {
+    // Otherwise if the password is present, it is added from POST array to $password variable
       $password = trim($_POST["pass"]);
     }
 
+    // Checks if password confirmation is empty after removing white space from password confirmation input
     if (empty(trim($_POST["confirmPass"]))) {
+    // If it is then a message is added to missing array
       $missing[] = "Confirm your password!";
     } else {
+    // Otherwise if password confirmation is present, it is added from POST array to $confirm_password variable
       $confirm_password = trim($_POST["confirmPass"]);
+    // A check is done between both password inputs; if they don't match then a message is added
       if ($password != $confirm_password) {
         $missing[] = "Passwords are not matching!";
       }
     }
 
+    // If no missing messages are present then if_block runs which creates an array of the player's date...
         if (count($missing) == 0) {
             // Create user array
-            $user = array(
+            $player = array(
               'username' => $username,
               'password' => $password,
               'score' => $score,
               'time' => $time
             );
       
-            // Save user to file
+    // And stores that data into a text on its own line.        
             $filename = "players.txt";
             $file = fopen($filename, "a");
-            fwrite($file, serialize($user)."\n");
+            fwrite($file, serialize($player)."\n");
             fclose($file);
       
-            // Redirect to login page
+    // Registration is complete and the player is redirected automatically to the index.php page to login
             header("location: index.php");
           }
         }
@@ -63,13 +74,14 @@
     <form method="post" name="register-form">
             <div class="loginBox">
                 <p id="loginTitle">Register</p>
+    <!-- This php block displays the missing messages if any are present -->
                 <?php if (count($missing) > 0) : 
                     ?>
                 <div>
-                    <?php foreach ($missing as $error) : 
+                    <?php foreach ($missing as $missing) : 
                         ?>
                     <p class="error">
-                    <?php echo $error; 
+                    <?php echo $missing; 
                     ?></p>
                     <?php endforeach; 
                     ?>
